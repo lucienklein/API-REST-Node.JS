264fc6f621ecde4c2d2443be128489caa52b78ca
Lucien Klein <44434935+lucienklein@users.noreply.github.com>
2019-11-14 17:01:26 +0100

    MAJ api

--- api/users.js
+++ api/users.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const checkAuth = require('./check-auth');
 const query = require('./query');
+// const checkAuth = require('./check-auth');
 
 //Les différentes routes et requetes possibles
-router.get('/', query.get_all);
+router.get('/:id', query.get);
 
-router.post('/signup', checkAuth, query.signup)
+router.get('/getName/:id', query.getName)
 
-router.post('/login', query.login)
+router.get('/getFull/:userEmail', query.getFull);
 
-router.get('/:userId', query.get_user);
+router.post('/newUser/', query.newUser);
 
-router.patch('/:userId', checkAuth, query.patch_user);
 
-router.delete('/:userId', checkAuth, query.delete_user);
+// router.get('/', query.get_all);
+
+// router.get('/login/:mail/:mdp', query.login)
+
+// router.patch('/:userId', checkAuth, query.patch_user);
+
+// router.delete('/:userId', checkAuth, query.delete_user);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 
 //Création du serveur
-const port = process.env.PORT || 8000;
+const port = 8080;
 const server = http.createServer(app);
 
 server.listen(port);
